c9e7cbd1ad577beb04e69a0f48fe070b408ca774
layne913 <enjoylaynelife@163.com>
2016-02-25 11:29:36 +0800

    finish the first exercise attribute class

--- js/script.js
+++ js/script.js
@@ -8,39 +8,43 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+	$("#before p").before("YOLO");
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css("font-size", "20px");
 });
 
 $("#css2 button").click(function() {
-	
+  $("#css2 p").css({"font-size" : "150%" ,
+                    "color" : "red"});
 });
 
 $("#attr1 button").click(function() {
-	
+    var attr = $("#attr1 p a").attr("href");
+    console.log(attr);
 });
 
 $("#attr2 button").click(function() {
-
+    $("#attr2 p a").attr("href", "www.google.com");
 });
 
 $("#class1 button").click(function() {
-	
+    $("#class1 p").addClass("bg-pimary");
 });
 
 $("#class2 button").click(function() {
-
+    var has = $("#class2 p").hasClass("lead");
+    alert(has);
 });
 
 $("#form1 button").click(function() {
-
+    var value = $("#exampleInputEmail1").val();
+    console.log(value);
 });
 
 $("#form2 button").click(function() {
-
+    var value = $("#exampleInputName2").val("hello");
 });
 
 // handle the mouseover event here
